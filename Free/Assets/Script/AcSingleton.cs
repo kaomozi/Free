1ee5bdadf732b2bcb295481d49d27c2c5e9e2a7f
seigoyoshida <seigo.yoshida@mixi.co.jp>
2020-10-20 19:31:00 +0900

    色々準備中

---
+++ Free/Assets/Script/AcSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class AcSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T m_instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                Type t = typeof(T);
+                m_instance = (T)FindObjectOfType(t);
+                if (m_instance == null)
+                {
+                    AcDebug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return m_instance;
+        }
+    }
+
+}
+
---
+++ Free/Assets/Script/Debug/AcDebug.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AcDebug 
+{
+    // Log
+    public static void Log(object log)
+    {
+        Debug.Log(log);
+    }
+
+    public static void LogWarning(object log)
+    {
+        Debug.LogWarning(log);
+    }
+
+    public static void LogError(object log)
+    {
+        Debug.LogError(log);        
+    }
+}
