791c0a5c1ab94e6daf596ffb93521bd28f6ffb47
seigoyoshida <seigo.yoshida@mixi.co.jp>
2020-10-20 19:31:50 +0900

    色々追加中

---
+++ Free/Assets/Script/Network/AcNetwork.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AcNetwork : AcSingleton<AcNetwork>
+{
+    bool m_isHost = false;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public bool StartHost()
+    {
+
+        return true;
+    }
+
+    public bool Connect(string ip)
+    {
+
+        return true;
+    }
+}
